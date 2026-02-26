53dad5df7a2f37aca8aec9fe96844cff27c3e704
Mubashir Chandle <264320902+mubashir-gwu@users.noreply.github.com>
2026-02-26 15:38:52 -0500

    Add label encoding logic

--- src/encoder/Encoder.java
+++ src/encoder/Encoder.java
@@ -3,14 +3,45 @@ package encoder;
 import java.util.*;
 
 public class Encoder {
+    private final static Map<String, Integer> labelsMap = new HashMap<>();
+
     public static void encode(String[] lines) {
+        // Clear the contents from any previous encoding.
+        labelsMap.clear();
+
         final List<TokenizedLine> tokenizedLines = getTokenizedLines(lines);
+
+        System.out.println("Tokenized Lines:");
         for (TokenizedLine tokenizedLine : tokenizedLines) {
             System.out.println(tokenizedLine);
+        }
+
+        System.out.println("================================================================================");
+
+        buildLabelsMap(tokenizedLines);
+        System.out.println("Labels Map:");
+        labelsMap.forEach((key, value) -> System.out.println(key + " -> " + value));
+    }
+
+    private static void buildLabelsMap(List<TokenizedLine> tokenizedLines) {
+        int counter = 0;
+
+        for (TokenizedLine tokenizedLine : tokenizedLines) {
+            if (tokenizedLine.opcode().equals("LOC")) {
+                counter = Integer.parseInt(tokenizedLine.operands()[0]);
+                continue;
+            }
+
+            if (!tokenizedLine.label().isEmpty()) {
+                labelsMap.put(tokenizedLine.label(), counter);
+            }
+            counter++;
         }
     }
 
     private static List<TokenizedLine> getTokenizedLines(String[] lines) {
+        // TODO: Skip blank lines.
+
         final List<TokenizedLine> tokenizedLines = new ArrayList<>();
 
         for (String line : lines) {
